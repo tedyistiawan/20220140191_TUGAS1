f4497c5fb4607cf957db6e6ce50703d8906071d7
tedyistiawan <tedypnd07@gmail.com>
2023-03-09 11:23:31 +0700

    TedyI

---
+++ example.cpp
@@ -0,0 +1,23 @@
+//library
+#include <iostream>
+using namespace std;
+
+//program main
+int main(){
+    //numeric p,l,luas
+    double p,l,luas;
+    //display "masukkan panjang"
+    cout<< "masukkan panjang = ";
+    //accept p
+    cin>>p;
+    //display "masukan lebar"
+    cout<<"masukkan lebar";
+    //accept L
+    cin>>l;
+    //compute luas=p*L
+    luas=p*l;
+    //display "luas persegi panjang adalah" ,luas
+    cout<<"luas persegi panjang adalah  "<<luas;
+
+}//end
+
